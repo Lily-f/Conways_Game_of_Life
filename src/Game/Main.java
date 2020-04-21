e680ede8b46ae37741ce04a7561b3d1f87a02e92
Mike Fahey <mikekiwi99@gmail.com>
2020-04-21 14:10:43 +1200

    Add main class

---
+++ src/Game/Main.java
@@ -0,0 +1,20 @@
+package Game;
+
+public class Main {
+    
+    /**
+     * Create board and start game
+     */
+    private Main(){
+    
+    }
+    
+    /**
+     * Process start
+     * @param args start arguments
+     */
+    public static void main(String[] args) {
+        new Main();
+    }
+
+}
