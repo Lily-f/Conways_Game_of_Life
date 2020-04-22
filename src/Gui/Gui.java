5f52099191681cdfe39ebfe06f6d573a18d4cfc3
Mike Fahey <mikekiwi99@gmail.com>
2020-04-22 18:57:35 +1200

    Add user/cell interaction and auto pause

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -31,22 +32,22 @@ public class Gui extends Application {
     /**
      * Minimum size of cells on the grid
      */
-    private static final int MIN_CELL_SIZE = 10;
+    private static final int MIN_CELL_SIZE = 15;
     
     /**
      * Initial width of game board
      */
-    private static final int DEFAULT_BOARD_WIDTH = 10;
+    private static final int DEFAULT_BOARD_WIDTH = 20;
     
     /**
      * Initial height of game board
      */
-    private static final int DEFAULT_BOARD_HEIGHT = 20;
+    private static final int DEFAULT_BOARD_HEIGHT = 10;
     
     /**
      * Delay between steps for board animation
      */
-    private static final int STEP_DELAY = 500;
+    private static final int STEP_DELAY = 200;
     
     /**
      * Game board
@@ -81,6 +82,8 @@ public class Gui extends Application {
         // Populate and format panes
         rootPane.setPadding(new Insets(25, 25, 25, 25));
         rootPane.getChildren().addAll(menu, boardGrid);
+        boardGrid.setVgap(1);
+        boardGrid.setHgap(1);
         setupGUIButtons(menu);
         createBoard();
         
@@ -106,6 +109,10 @@ public class Gui extends Application {
         // Run button to loop the game
         Button runBtn = new Button("Run");
         runBtn.setOnAction((e) -> {
+            // Do nothing if already running
+            if(running) return;
+            
+            // Else start a reoccurring event
             running = true;
             new Timer().schedule(
                 new TimerTask() {
@@ -131,9 +138,6 @@ public class Gui extends Application {
      */
     private void createBoard() {
         
-        //todo remove
-        board.setCellLife(2, 2, Cell.ALIVE);
-        
         // Loop through each cell in the board, adding them to the grid
         for(int y = 0; y < board.getHeight(); y ++){
             for(int x = 0; x < board.getWidth(); x ++){
@@ -141,22 +145,43 @@ public class Gui extends Application {
                 Rectangle cellBox = new Rectangle(0, 0, MIN_CELL_SIZE, MIN_CELL_SIZE);
                 if(board.getCell(x, y).isAlive())   cellBox.setFill(Color.BLACK);
                 else    cellBox.setFill(Color.WHITE);
-                boardGrid.add(cellBox, y, x);
+                boardGrid.add(cellBox, x, y);
             }
         }
+        
+        // Set clicked cells to alive
+        boardGrid.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            Node clickedNode = event.getPickResult().getIntersectedNode();
+            if(clickedNode == null || clickedNode == boardGrid) return;
+            int x = GridPane.getColumnIndex(clickedNode);
+            int y = GridPane.getRowIndex(clickedNode);
+            board.setCellLife(x, y, Cell.ALIVE);
+            updateBoard();
+        });
     }
     
     /**
-     * Update the board display
+     * Update the board display and check that a cell is alive
      */
     private void updateBoard(){
+        
+        boolean cellsAlive = false;
+        
         ObservableList<Node> cellBoxes = boardGrid.getChildren();
         for (Node cellBox : cellBoxes) {
-            int x = GridPane.getRowIndex(cellBox);
-            int y = GridPane.getColumnIndex(cellBox);
+            int x = GridPane.getColumnIndex(cellBox);
+            int y = GridPane.getRowIndex(cellBox);
             
-            if(board.getCell(x, y).isAlive())   ((Rectangle)cellBox).setFill(Color.BLACK);
-            else    ((Rectangle)cellBox).setFill(Color.WHITE);
+            if(board.getCell(x, y).isAlive()){
+                ((Rectangle)cellBox).setFill(Color.BLACK);
+                cellsAlive = true;
+            }
+            else{
+                ((Rectangle)cellBox).setFill(Color.WHITE);
+            }
         }
+        
+        // Pause the game if no cells are alive
+        if(!cellsAlive) running = false;
     }
 }
